e16994b12c96d6e270f78af1a968bec6a5df6c9e
unknown <anandraman.job@gmail.com>
2021-03-05 07:22:43 +0530

    first commit

---
+++ src/test/java/com/springboot/javatechie/JavaTechieBuildingDockerImagesUsingJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.javatechie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaTechieBuildingDockerImagesUsingJenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
